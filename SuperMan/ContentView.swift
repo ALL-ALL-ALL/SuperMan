03239ab6ff9088b725703f5312fe7cd5f0176e3f
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-06 01:48:01 +0200

    ✅

--- SuperMan/ContentView.swift
+++ SuperMan/ContentView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var location: CGPoint = CGPoint(x: 200, y: 400)
+//    @State private var fingerLocation: CGPoint? // indice visuel avec un cercle vert quand jappuie
+
+        
+    var simpleDrag: some Gesture {
+            DragGesture()
+                .onChanged { value in
+                    self.location = value.location
+                } // fin changed
+        } // fin du geste
+    
+    
+//    var fingerDrag: some Gesture { // 2
+//             DragGesture()
+//                .onChanged { value in
+//                    self.fingerLocation = value.location
+//                }
+//                .onEnded { value in
+//                    self.fingerLocation = nil
+//                }
+//        }
+
     var body: some View {
-        VStack {
-            
-            Text("Hello, world!!!!!")
-                .foregroundColor(.teal)
-                .font(.system(size: 60))
-        } // fin vstack
+        ZStack {
+            Color(.black)
+            VStack {
+
+                Image("s")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300)
+                    .position(location) // ou c'est
+                    .gesture(simpleDrag) // pour le deplacement
+                
+                
+                
+                
+//                    .gesture(simpleDrag.simultaneously(with: fingerDrag))
+//                                if let fingerLocation = fingerLocation {
+//                                    Circle()
+//                                        .stroke(Color.green, lineWidth: 10)
+//                                        .frame(width: 100, height: 100)
+//                                        .position(fingerLocation)
+//                                        .padding(.top,-430)
+//                                }
+                            
+   
+            }// fin vstack
+        } // fin Zstack
+        .ignoresSafeArea()
+        
         
         
         
