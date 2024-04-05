da346db4ce15a407c6f0e4cb58f1ef28bb67492d
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-06 00:03:59 +0200

    Initial Commit

---
+++ SuperMan/SuperManApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperManApp.swift
+//  SuperMan
+//
+//  Created by  Ixart on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
